fe26536a9754f54f8e69803c5fecd663a4cecb7e
MacMini2012 <macmini2012@MacMini2012s-Mac-mini.local>
2019-12-09 15:24:52 +0700

    add pageViewController

---
+++ MovieReviewApp/MovieReviewApp/DetailPages/ExtensionDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionDetailViewController.swift
+//  MovieReviewApp
+//
+//  Created by MacMini2012 on 12/9/19.
+//  Copyright © 2019 MGMVVMRxSwiftDemo. All rights reserved.
+//
+
+import Foundation
